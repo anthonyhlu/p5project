6417a168b0bdb146dc9ff0798034588fc5a0efc1
Anthony Lu <anthonylu@berkeley.edu>
2022-02-16 03:36:19 -0800

    cow button update

--- sketch.js
+++ sketch.js
@@ -6,16 +6,20 @@ let pigImg;
 let penguinImg;
 
 function preload() {
-  
+  cowImg = createImg("https://i.imgur.com/yf0MajD.jpg"); 
+  beeImg = loadImage("https://i.imgur.com/h2mt3sI.jpg");
+  penguinImg = loadImage("https://i.imgur.com/6CdUJfY.jpg");
+
 }
 
 function setup() {
-  createCanvas(750, 500);
+  createCanvas(1000, 1000);
   nextStepButton = createButton("next step");
   nextStepButton.position(750, 850);
   nextStepButton.size(300)
   nextStepButton.mousePressed();
   drawCowButton = createButton("draw a cow");
+  drawCowButton.mousePressed(drawCow);
   drawCowButton.position(200, 620);
   drawCowButton.size(200);
   drawBeeButton = createButton("draw a bee");
@@ -24,16 +28,19 @@ function setup() {
   drawPenguinButton = createButton("draw a penguin");
   drawPenguinButton.position(1400, 620);
   drawPenguinButton.size(200);
-  cowImg = loadImage("cow.jpg");
-  
+  nextStepButton.style("font",'comic-sans');
+  cowImg.mousePressed(drawCow)
+  cowImg.position(150, 250);
+  image(beeImg, 700, 250);
+  image(penguinImg, 1300, 250);
+
+}
+
+function drawCow() {
+  ellipse(100, 100, 100, 100);
 }
 
 function draw() {
-  nextStepButton.display();
-  cowButton.display();
-  beeImg.display();
-  penguinImg.display();
-  image(cowImg, 0, 0);
 
 }
 
