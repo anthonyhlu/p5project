786bbcc2c91ed0eb633caec08e97ccdf8aabc1d2
Anthony Lu <anthonylu@berkeley.edu>
2022-02-15 16:49:07 -0800

    basic html css

--- sketch.js
+++ sketch.js
@@ -1,9 +1,21 @@
+var cow, chicken, pig, penguin
+var nextStepButton
+var bkcolor
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(750, 500);
+  bk = 'rgba(0,255,0, 0.25)';
+  text("draw like ant", 100, 100)
+  textSize(100)
+  nextStepButton = createButton("next step")
+  nextStepButton.position(750/3, 500);
+  nextStepButton.size(300)
 }
 
 function draw() {
-  background(220);
+
+  
+
 }
 
-#test2
