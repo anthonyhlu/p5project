ac1ba2212a767b77d47cc510895df9a6bbbf2876
Anthony Lu <anthonylu@berkeley.edu>
2022-02-15 20:51:27 -0800

    pictureless app

--- sketch.js
+++ sketch.js
@@ -1,21 +1,41 @@
-var cow, chicken, pig, penguin
-var nextStepButton
-var bkcolor
+let cowButton, chickenButton, pigButton, penguinButton;
+let nextStepButton, drawCowButton, drawPigBbutton, drawPenguinButton, drawBeeButton;
+var cowImg;
+let chickenImg;
+let pigImg;
+let penguinImg;
 
+function preload() {
+  
+}
 
 function setup() {
   createCanvas(750, 500);
-  bk = 'rgba(0,255,0, 0.25)';
-  text("draw like ant", 100, 100)
-  textSize(100)
-  nextStepButton = createButton("next step")
-  nextStepButton.position(750/3, 500);
+  nextStepButton = createButton("next step");
+  nextStepButton.position(750, 850);
   nextStepButton.size(300)
+  nextStepButton.mousePressed();
+  drawCowButton = createButton("draw a cow");
+  drawCowButton.position(200, 620);
+  drawCowButton.size(200);
+  drawBeeButton = createButton("draw a bee");
+  drawBeeButton.position(800, 620);
+  drawBeeButton.size(200);
+  drawPenguinButton = createButton("draw a penguin");
+  drawPenguinButton.position(1400, 620);
+  drawPenguinButton.size(200);
+  cowImg = loadImage("cow.jpg");
+  
 }
 
 function draw() {
-
-  
+  nextStepButton.display();
+  cowButton.display();
+  beeImg.display();
+  penguinImg.display();
+  image(cowImg, 0, 0);
 
 }
+
+
 
